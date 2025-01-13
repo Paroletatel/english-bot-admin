d9c033a82b7be7337db1306243c405e15c2819c6
reginakhestia <regina25081999@gmail.com>
2025-01-13 18:19:28 +0300

    no kodac

--- src/index.ts
+++ src/index.ts
@@ -15,62 +15,12 @@ bot.on('channel_post', async (msg) => {
     // Проверяем, есть ли в сообщении видео
     if (msg.video) {
         try {
-            // Скачиваем видео
-            const filePath = await bot.downloadFile(msg.video.file_id, './');
-            const inputFilePath = path.resolve(filePath);
-            const outputFilePath = `${inputFilePath}_converted.mp4`;
+            const videoFileId = msg.video.file_id;
+            bot.sendMessage(chatId, `${videoFileId}`);
 
-            // Проверяем кодеки видео
-            ffmpeg.ffprobe(inputFilePath, async (err, metadata) => {
-                if (err) {
-                    throw err;
-                }
-
-                const hasH264 = metadata.streams.some((stream) => 
-                    stream.codec_name === 'h264' && stream.codec_type === 'video');
-
-                if (!hasH264) {
-                    await convertVideoToH264(inputFilePath, outputFilePath);
-
-                    await bot.sendMessage(chatId, `Видео сконвертировано, перешлите его в этот канал для получения id\n⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️`);
-                    await bot.sendVideo(chatId, outputFilePath);
-
-                    // Удаляем временные файлы
-                    fs.unlinkSync(inputFilePath);
-                    fs.unlinkSync(outputFilePath);
-                } else {
-                    const videoFileId = msg.video.file_id;
-                    bot.sendMessage(chatId, `${videoFileId}`);
-                    
-                    // Удаляем временные файлы
-                    fs.unlinkSync(inputFilePath);
-                }
-            });
         } catch (error) {
             console.error('Ошибка обработки видео:', error);
             bot.sendMessage(chatId, 'Произошла ошибка при обработке видео.');
         }
     }
 });
-
-async function convertVideoToH264(inputPath: string, outputPath: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-        ffmpeg(inputPath)
-            .outputOptions([
-                '-c:v libx264',  // Конвертация видео в H.264
-                '-preset fast',  // Быстрый пресет
-                '-crf 22'        // Константа качества (0 - наилучшее качество, 51 - наихудшее)
-            ])
-            .on('end', resolve)
-            .on('error', reject)
-            .save(outputPath);
-    });
-}
-
-// bot.getUpdates().then(updates => {
-//     updates.forEach(update => {
-//         if (update.channel_post && update.channel_post.chat) {
-//             console.log(update.channel_post.chat.id);
-//         }
-//     });
-// });
